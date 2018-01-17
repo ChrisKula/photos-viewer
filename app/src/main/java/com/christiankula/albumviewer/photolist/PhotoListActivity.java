17f6f38dec8e084eff9be0fd7c6ce873123f751c
Christian KULA <christian.kula.ck@gmail.com>
2018-01-17 11:02:12 +0100

    Rename 'MainActivity' to 'PhotoListActivity'

---
+++ app/src/main/java/com/christiankula/albumviewer/photolist/PhotoListActivity.java
@@ -0,0 +1,15 @@
+package com.christiankula.albumviewer.photolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.christiankula.albumviewer.R;
+
+public class PhotoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_list);
+    }
+}
