4d8792a53b8f2c2aa77f58ffdae0728cd987ed38
Christian KULA <christian.kula.ck@gmail.com>
2018-01-19 13:11:39 +0100

    Update Persistence module with Room database providing methods

--- app/src/main/java/com/christiankula/albumviewer/injection/modules/PersistenceModule.java
+++ app/src/main/java/com/christiankula/albumviewer/injection/modules/PersistenceModule.java
@@ -1,9 +1,12 @@
 package com.christiankula.albumviewer.injection.modules;
 
+import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.christiankula.albumviewer.persistence.AlbumViewerDatabase;
+import com.christiankula.albumviewer.persistence.PhotoDao;
 import com.christiankula.albumviewer.persistence.SharedPreferencesHelper;
 
 import javax.inject.Singleton;
@@ -35,5 +38,27 @@ public class PersistenceModule {
     @Singleton
     SharedPreferencesHelper provideSharedPreferencesHelper(SharedPreferences sharedPreferences) {
         return new SharedPreferencesHelper(sharedPreferences);
+    }
+
+    /**
+     * Provides a singleton instance of {@link AlbumViewerDatabase}
+     *
+     * @param context the application context
+     */
+    @Provides
+    @Singleton
+    AlbumViewerDatabase provideApplicationDatabase(Context context) {
+        return Room.databaseBuilder(context, AlbumViewerDatabase.class, AlbumViewerDatabase.NAME).build();
+    }
+
+    /**
+     * Provides a singleton instance of {@link PhotoDao}
+     *
+     * @param albumViewerDb the application's database
+     */
+    @Provides
+    @Singleton
+    PhotoDao providePhotoDao(AlbumViewerDatabase albumViewerDb) {
+        return albumViewerDb.photoDao();
     }
 }
